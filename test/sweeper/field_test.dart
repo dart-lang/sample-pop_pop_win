571aed2b9fbe0f98ce6aa7c495a4acba0cbe0686
Kevin Moore <kevin@thinkpixellab.com>
2012-08-11 18:07:21 -0400

    test oops

--- test/sweeper/field_test.dart
+++ test/sweeper/field_test.dart
@@ -7,15 +7,9 @@ class FieldTest {
   static void _testDefaults() {
     final f = new Field();
 
-    int mineCount = 0;
-    for(int x = 0; x < 16; x++) {
-      for(int y = 0; y < 16; y++) {
-        if(f.isMine(x, y)) {
-          mineCount++;
-        }
-      }
-    }
-    expect(mineCount, equals(f.mineCount));
+    expect(f.mineCount, equals(40));
+    expect(f.rows, equals(16));
+    expect(f.cols, equals(16));
   }
 
   static void _testMineCount() {
