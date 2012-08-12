0d68831caeb17bcd9b1166eb7489f0026efd9903
Kevin Moore <kevin@thinkpixellab.com>
2012-08-12 13:32:25 -0400

    Game: implemented and tested setFlag

--- lib/sweeper/game.dart
+++ lib/sweeper/game.dart
@@ -1,22 +1,53 @@
 class Game {
   final Field field;
+  final List<SquareState> _states;
 
   GameState _gameState;
   int _minesLeft;
 
   Game(this.field) :
-    _gameState = GameState.notStarted {
+    _gameState = GameState.notStarted,
+    _states = new List<SquareState>() {
     assert(field != null);
     _minesLeft = field.mineCount;
+    _states.insertRange(0, field.size, SquareState.hidden);
   }
 
   int get minesLeft => _minesLeft;
 
   GameState get state => _gameState;
 
-  void setFlag(int x, int y) {
+  SquareState getSquareState(int x, int y) {
+    final i = field._getIndex(x, y);
+    return _states[i];
+  }
+
+  void setFlag(int x, int y, bool value) {
+    _ensureStarted();
+    assert(value != null);
+    final i = field._getIndex(x, y);
+
+    final currentSS = _states[i];
+    if(value) {
+      assert(currentSS == SquareState.hidden);
+      _states[i] = SquareState.flagged;
+      _minesLeft--;
+    } else {
+      assert(currentSS == SquareState.flagged);
+      _states[i] = SquareState.hidden;
+      _minesLeft++;
+    }
   }
 
   void reveal(int x, int y) {
+    _ensureStarted();
+    final i = field._getIndex(x, y);
+  }
+
+  void _ensureStarted() {
+    if(state == GameState.notStarted) {
+      _gameState = GameState.started;
+    }
+    assert(state == GameState.started);
   }
 }
--- test/sweeper/game_test.dart
+++ test/sweeper/game_test.dart
@@ -2,13 +2,42 @@ class GameTest {
   static void run() {
     group('Game', () {
       test('initial values', _testInitial);
+      test('setFlag', _testSetFlag);
+      test('cannot reveal flagged', _testCannotRevealFlagged);
+      test('cannot flag revealed', _testCannotFlagRevealed);
     });
   }
 
   static void _testInitial() {
-    final g = new Game(FieldTest.getSampleField());
+    final f = FieldTest.getSampleField();
+    final g = new Game(f);
 
     expect(g.minesLeft, equals(13));
     expect(g.state, equals(GameState.notStarted));
+
+    for(int x = 0; x < f.cols; x++) {
+      for(int y = 0; y < f.rows; y++) {
+        expect(g.getSquareState(x,y), equals(SquareState.hidden));
+      }
+    }
+  }
+
+  static void _testSetFlag() {
+    final g = new Game(FieldTest.getSampleField());
+
+    expect(g.getSquareState(0,0), equals(SquareState.hidden));
+    g.setFlag(0, 0, true);
+    expect(g.getSquareState(0,0), equals(SquareState.flagged));
+    expect(g.minesLeft, equals(12));
+    expect(g.state, equals(GameState.started));
+  }
+
+  static void _testCannotRevealFlagged() {
+  }
+
+  static void _testCannotFlagRevealed() {
+  }
+
+  static void _testWin() {
   }
 }
