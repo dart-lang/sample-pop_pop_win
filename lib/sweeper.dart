f7f8e2450cc57525e2da36f4e2f8cbe3edfb4679
Kevin Moore <kevin@thinkpixellab.com>
2012-08-12 22:54:04 -0400

    moving to use dart lib...for now...a bit

--- lib/sweeper.dart
+++ lib/sweeper.dart
@@ -1,14 +1,9 @@
 #library('sweeper');
 
 #import('dart:math', prefix:'math');
+#import('../../dartlib/lib/dartlib.dart');
 
 #source('sweeper/field.dart');
 #source('sweeper/game.dart');
 #source('sweeper/game_state.dart');
 #source('sweeper/square_state.dart');
-
-void require(bool truth, [String message = '']) {
-  if(!truth) {
-    throw new Exception(message);
-  }
-}
