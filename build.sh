4ffb52171ee4e592ac9be416043598363b94aaf1
Kevin Moore <kevin@thinkpixellab.com>
2012-08-29 10:19:30 -0400

    build both apps, verbose output

--- build.sh
+++ build.sh
@@ -2,12 +2,20 @@
 PACK_DIR=`pwd`/packages/
 echo $DART_SDK
 dart2js='lib/dart2js/lib/compiler/implementation/dart2js.dart'
-cmd="dart
+core_cmd="dart
 --heap_growth_rate=32
 --package-root=$PACK_DIR
 $DART_SDK/$dart2js
 --minify
---out=./html/sweeper_dom.dart.js
-./html/sweeper_dom.dart"
-echo $cmd
-exec $cmd
+-v"
+
+function build {
+  echo "Building $1"
+  cmd="$core_cmd --out=$1.js $1"
+  echo $cmd
+  eval $cmd
+  echo
+}
+
+build './html/sweeper_dom.dart'
+build './html/canvas/sweeper_canvas.dart'
