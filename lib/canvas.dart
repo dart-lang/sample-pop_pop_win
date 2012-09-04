61bab9eaa1fc5de06b65c65b19863169dfa9305f
Kevin Moore <kevin@thinkpixellab.com>
2012-09-04 18:12:25 -0400

    starting to wire up texture logic

--- lib/canvas.dart
+++ lib/canvas.dart
@@ -14,3 +14,47 @@
 #source('canvas/image_loader.dart');
 
 #source('texture/texture_input.dart');
+
+Map<String, TextureInput> _textures;
+
+void populateTextures(Map<String, TextureInput> textures) {
+  assert(_textures == null);
+  assert(textures != null);
+  _textures = textures;
+}
+
+TextureInput getTexture(String key) {
+  assert(_textures != null);
+  return _textures[key];
+}
+
+void drawTextureAt(CanvasRenderingContext2D ctx, String textureKey,
+                   dartlib.Coordinate location, ImageElement imageElement) {
+  final texture = getTexture(textureKey);
+  _drawTextureAt(ctx, location, texture, imageElement);
+}
+
+void _drawTextureAt(CanvasRenderingContext2D ctx, dartlib.Coordinate location,
+                    TextureInput texture, ImageElement imageElement) {
+  ctx.save();
+  final tx = new dartlib.AffineTransform();
+  tx.translate(location.x, location.y);
+
+  var theFrame = texture.frame;
+  var offset = texture.offset;
+  tx.translate(offset.x, offset.y);
+
+  if(texture.rotated) {
+    tx.rotate(-PI/2.0, 0, 0);
+    theFrame = new dartlib.Rect(theFrame.left, theFrame.top,
+        theFrame.height, theFrame.width);
+  }
+
+  final imgSize = theFrame.size.toVector().scale(-0.5);
+  tx.translate(imgSize.x, imgSize.y);
+
+  CanvasUtil.transform(ctx, tx);
+
+  CanvasUtil.drawImage(ctx, imageElement, theFrame);
+  ctx.restore();
+}
