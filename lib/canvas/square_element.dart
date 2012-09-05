2130224cb2f7dcbe59a73b15bfccfeb6429d08ef
Kevin Moore <kevin@thinkpixellab.com>
2012-09-05 13:18:52 -0400

    drawing adjacent counts

--- lib/canvas/square_element.dart
+++ lib/canvas/square_element.dart
@@ -1,6 +1,12 @@
 class SquareElement extends PElement {
   static const int _size = 80;
   static const String _textureName = "balloon.png";
+  static const List<String> _numberMap = const["game_board_center",
+                                               "number_one", "number_two",
+                                               "number_three", "number_four",
+                                               "number_five", "number_six",
+                                               "number_seven", "number_eight"];
+
   final int x, y;
 
   SquareElement(this.x, this.y) : super(_size, _size) {
@@ -12,6 +18,11 @@ class SquareElement extends PElement {
       drawTextureAt(ctx, _textureName,
           const dartlib.Coordinate(_size ~/ 2, _size ~/ 2));
 
+    } else if(_squareState == SquareState.revealed) {
+      final adjCount = _adjacentCount;
+      final textureName = _numberMap[adjCount];
+      drawTextureAt(ctx, "$textureName.png",
+          const dartlib.Coordinate(_size ~/ 2, _size ~/ 2));
     } else {
       ctx.fillStyle = _fillStyle;
       ctx.fillRect(0, 0, width, height);
@@ -20,17 +31,7 @@ class SquareElement extends PElement {
       ctx.strokeRect(0.5, 0.5, width, height, 1);
     }
 
-    if(_squareState == SquareState.revealed) {
-      final adjCount = _adjacentCount;
-      if(adjCount > 0) {
-        final halfSize = _size ~/ 2;
-        ctx.textBaseline = 'middle';
-        ctx.font = '${halfSize}px Helvetica';
-        ctx.fillStyle = 'black';
-        ctx.textAlign = 'center';
-        ctx.fillText(adjCount.toString(), halfSize, halfSize, _size);
-      }
-    }
+
 
   }
 
@@ -49,16 +50,12 @@ class SquareElement extends PElement {
     switch(_squareState) {
       case SquareState.flagged:
         return 'orange';
-      case SquareState.revealed:
-        return 'white';
       case SquareState.mine:
         return 'red';
       case SquareState.safe:
         return 'green';
-      case SquareState.hidden:
-        return '#6666CC';
       default:
-        throw 'not supported';
+        throw 'not supported - $_squareState';
     }
   }
 }
