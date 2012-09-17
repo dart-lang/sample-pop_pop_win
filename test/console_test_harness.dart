60396cee483448564b9180ed418772799773c7e7
Kevin Moore <kevin@thinkpixellab.com>
2012-09-17 09:49:21 -0400

    group SEP FTW

--- test/console_test_harness.dart
+++ test/console_test_harness.dart
@@ -1,9 +1,11 @@
+#import('package:unittest/unittest.dart');
 #import('package:unittest/vm_config.dart');
 
 #import('sweeper/_sweeper_runner.dart');
 
 main() {
   useVmConfiguration();
+  groupSep = ' - ';
 
   runSweeperTests();
 }
