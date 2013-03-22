3d489ca47b3074f6ae3d112d5c9586736d08faaf
Kevin Moore <kevin@thinkpixellab.com>
2013-03-22 18:27:16 -0400

    Disabling in-game analytics until SDK 9283 is fixed

--- lib/platform_web.dart
+++ lib/platform_web.dart
@@ -13,8 +13,12 @@ class PlatformWeb extends PlatformTarget {
   Map<String, String> get storage => window.localStorage;
 
   void trackAnalyticsEvent(String category, String action, [String label, int value]) {
+    /*
+     * TODO: DARTBUG: https://code.google.com/p/dart/issues/detail?id=9283
+     * Commenting out analytics tracking until this guy is fixed
     js.scoped((){
       js.context['pushAnalytics'](js.array(['_trackEvent', category, action, label, value]));
     });
+    */
   }
 }
