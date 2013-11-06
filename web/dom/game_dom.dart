4ea2001550949866dc59e5c4dc27efd8987670ac
Kevin Moore <kevin@thinkpixellab.com>
2013-11-06 12:16:41 -0300

    tiny tweaks to DOM game

--- web/dom/game_dom.dart
+++ web/dom/game_dom.dart
@@ -1,15 +1,15 @@
 import 'dart:html';
 import 'package:poppopwin/html.dart';
 
-main(){
+void main() {
   final int w = 16, h = 16, m = 40;
 
   final TableElement poppopwinTable = querySelector('#gameTable');
   final Element bombsLeftDiv = querySelector('#bombsLeft');
   final Element gameStateDiv = querySelector('#gameState');
   final Element clockDiv = querySelector('#clock');
-  final gameView = new GameView(w, h, m,
-      poppopwinTable, bombsLeftDiv, gameStateDiv, clockDiv);
+  final gameView = new GameView(w, h, m, poppopwinTable, bombsLeftDiv,
+      gameStateDiv, clockDiv);
 
   final DivElement highScoreDiv = querySelector('#highScore');
   final highScoreView = new HighScoreView(gameView, highScoreDiv);
