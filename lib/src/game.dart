bacaa1994ee4aadeeda58b7d2bb062dab5f00e01
Kevin Moore <kevmoo@google.com>
2018-04-27 13:02:48 -0700

    Renaming lib/src/game/game.dart -> game_core.dart
    Work-around for https://youtrack.jetbrains.com/issue/WEB-32348

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -3,6 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 export 'game/field.dart';
-export 'game/game.dart';
+export 'game/game_core.dart';
 export 'game/game_state.dart';
 export 'game/square_state.dart';
